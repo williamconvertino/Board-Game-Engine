6dd7419b6e37010c844ae21dc16122bbccf60103
William Convertino <wac20@duke.edu>
2021-11-13 16:08:14 -0500

    Updated card class

--- src/ooga/model/data/cards/Card.java
+++ src/ooga/model/data/cards/Card.java
@@ -1,22 +1,41 @@
 package ooga.model.data.cards;
 
 
+import java.lang.reflect.Method;
+import java.util.List;
 import ooga.model.data.player.Player;
 
 /**
- * A class to represent any non-property card and its action.
+ * A class to represent the game's action cards.
+ *
+ * @author William Convertino
+ *
+ * @since 0.0.1
  */
-public abstract class Card {
+public class Card {
 
-    /**
-     * Default constructor
-     */
-    public Card() {
+    //The name of the card.
+    private String myName;
+
+    //A description of the card.
+    private String myDescription;
+
+    //A list of commands to execute. //TODO: Make the command list functional.
+    private List<Method> commandList;
+
+    public Card(String myName, String myDescription, List<Method> commandList) {
+        this.myName = myName;
+        this.myDescription = myDescription;
+        this.commandList = commandList;
     }
 
     /**
-     * 
+     * Executes the card's command list with the active player as the subject.
+     *
+     * @param player the player who is executing the card.
      */
-    public abstract void execute(Player player);
+    public void execute(Player player) {
+
+    }
 
 }
