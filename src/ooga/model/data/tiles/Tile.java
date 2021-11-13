5a1f681bc6d62ad3fe00bfde278669a9345c28bd
William Convertino <wac20@duke.edu>
2021-11-13 15:20:03 -0500

    Updated Tile class

--- src/ooga/model/data/tiles/Tile.java
+++ src/ooga/model/data/tiles/Tile.java
@@ -3,7 +3,8 @@ package ooga.model.data.tiles;
 import ooga.model.data.player.Player;
 
 /**
- * This class represents a tile on the board. A player can pass through or land on a tile when moving, which is reflected by the executePassThrough and executeLandOn methods.
+ * This class represents a tile on the board.A player can pass through or land on a tile when moving,
+ * which is reflected by the executePassThrough and executeLandOn methods.
  * 
  * @author William Convertino
  * 
@@ -11,23 +12,36 @@ import ooga.model.data.player.Player;
  */
 public abstract class Tile {
 
+    private String name;
+
     /**
-     * Default constructor
+     * Constructs a new tile with the specified name.
+     *
+     * @param name the name of the tile.
      */
-    public Tile() {
+    public Tile(String name) {
+        this.name = name;
     }
 
     /**
-     * @param player 
-     * @param
+     * Executes whenever a player passes through the tile.
+     *
+     * @param player the player who is passing through the tile.
      */
     public abstract void executePassThrough(Player player);
 
     /**
-     * @param player
+     * Executes whenever a player lands on a tile.
+     *
+     * @param player the player who landed on the tile.
      */
     public abstract void executeLandOn(Player player);
 
+    /**
+     * Returns the name of the tile.
+     *
+     * @return the name of the tile.
+     */
     public abstract String getName();
 
 }
