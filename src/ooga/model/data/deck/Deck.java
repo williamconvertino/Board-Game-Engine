256d0c9ef1ad8c79211986d3d27b060bbabcbb6a
William Convertino <wac20@duke.edu>
2021-11-13 16:17:56 -0500

    Updated Deck class

--- src/ooga/model/data/deck/Deck.java
+++ src/ooga/model/data/deck/Deck.java
@@ -1,5 +1,8 @@
 package ooga.model.data.deck;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import ooga.model.data.cards.Card;
 
@@ -12,34 +15,48 @@ import ooga.model.data.cards.Card;
  */
 public class Deck {
 
-    /**
-     * Default constructor
-     */
-    public Deck() {
+    //The name of the deck.
+    private String myName;
+
+    //A list of all the cards that belong to the deck.
+    private List<Card> myCardList;
+
+    //A list of all the cards that are available to be drawn.
+    private LinkedList<Card> availableCards;
+
+    public Deck(String myName, List<Card> myCards) {
+        this.myName = myName;
+        this.myCardList = myCards;
+        resetDeck();
     }
 
     /**
-     * 
-     */
-    private List<Card> cards;
-
-
-    /**
-     * 
-     */
-    public void shuffle() {
-
-    }
-
-    /**
-     * @return
+     *  Removes a random card from the deck and returns it.
+     *
+     * @return a random card from the deck.
      */
     public Card drawCard() {
-        return null;
+        return availableCards.pop();
     }
 
+    //Resets the deck so that it contains each of the cards in myCardList, then shuffles.
+    private void resetDeck() {
+        List<Card> myCards = new ArrayList<>();
+        Collections.copy(myCardList, myCards);
+        Collections.shuffle(myCards);
+        availableCards = new LinkedList<>(myCards);
+    }
+
+    /**
+     * Returns true if the deck is out of cards, and false otherwise.
+     *
+     * @return true if the deck is out of cards, and false otherwise.
+     */
     public boolean isEmpty(){
-        return true;
+        if (availableCards.isEmpty()) {
+            resetDeck();
+        }
+        return availableCards.isEmpty();
     }
 
 }
