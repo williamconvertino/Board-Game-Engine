24e27c1b7b9e1c6198c2170c8173b35988587368
Casey Goldstein <cmg95@duke.edu>
2021-11-15 20:19:49 -0500

    added property files, adjusted parser test

--- test/ooga/model/game_handling/ConfigParserTest.java
+++ test/ooga/model/game_handling/ConfigParserTest.java
@@ -4,12 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import ooga.model.data.properties.Property;
 import org.junit.jupiter.api.Test;
 
 public class ConfigParserTest extends GameHandlingTest{
   @Test
   void testParseProperty() throws IOException {
     ConfigParser myParser = new ConfigParser();
-    myParser.parseProperties("monopoly_original/board/properties");
+    ArrayList<Property> propertyList = myParser.parseProperties("monopoly_original/board/properties");
+    for (Property prop: propertyList){
+      System.out.println(prop.getName());
+    }
+
   }
 }
