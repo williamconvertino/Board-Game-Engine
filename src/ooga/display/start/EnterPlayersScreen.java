9e8185405213b83ccbdde3677bbbba31427c39f8
Henry Huynh <89464231+henryhuynh111@users.noreply.github.com>
2021-11-19 18:10:43 -0800

    working on enter player screen

---
+++ src/ooga/display/start/EnterPlayersScreen.java
@@ -0,0 +1,60 @@
+package ooga.display.start;
+
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import ooga.display.Display;
+import ooga.display.DisplayManager;
+import ooga.display.ui_tools.LanguageUI;
+import ooga.display.ui_tools.UIBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class EnterPlayersScreen extends Display {
+    private BorderPane startMenu;
+    private Stage myStage;
+    private DisplayManager myDisplayManager;
+    private UIBuilder myBuilder;
+    private ResourceBundle myLangResource;
+    private LanguageUI myLanguageUI;
+    private static final String DEFAULT_RESOURCE_PACKAGE = Display.class.getPackageName() + ".resources.";
+    private static final String STYLE_PACKAGE = "/" + DEFAULT_RESOURCE_PACKAGE.replace(".", "/");
+    private static final String DEFAULT_STYLE = STYLE_PACKAGE + "mainmenu.css";
+    private Scene scene;
+
+    public EnterPlayersScreen (Stage stage, DisplayManager displayManager, ResourceBundle langResource) {
+        myLangResource = langResource;
+        myBuilder = new UIBuilder(langResource);
+        myStage = stage;
+        myDisplayManager = displayManager;
+        startMenu = new BorderPane();
+        startMenu.setTop(myBuilder.makeLabel("EnterPlayerNames"));
+        startMenu.setLeft(makeTextAreas());
+        makeScene();
+    }
+
+    private Node makeTextAreas() {
+        TextArea area = new TextArea("Enter Player Name");
+    }
+
+
+    private void makeScene() {
+        scene = new Scene(startMenu, 800, 600);
+        scene.getStylesheets().add(DEFAULT_STYLE);
+    }
+
+    /**
+     * Get the scene
+     * @return scene
+     */
+    @Override
+    public Scene getScene() {
+        return scene;
+    }
+
+}
--- src/ooga/display/ui_tools/UIBuilder.java
+++ src/ooga/display/ui_tools/UIBuilder.java
@@ -186,4 +186,9 @@ public class UIBuilder {
         return tab;
     }
 
+    public TextArea makeTextArea(String id) {
+        TextArea text = new TextArea(id);
+        text.setId(id);
+    }
+
 }
