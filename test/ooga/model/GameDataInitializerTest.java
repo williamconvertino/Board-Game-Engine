60be3094ce03588d6254fa90fab0c8a679f908e7
Casey Goldstein <cmg95@duke.edu>
2021-11-28 14:59:19 -0500

    added gamedata initializer tests

---
+++ test/ooga/model/GameDataInitializerTest.java
@@ -0,0 +1,19 @@
+package ooga.model;
+
+import ooga.GameDataInitializer;
+import ooga.exceptions.ImproperlyFormattedFile;
+import ooga.model.data.gamedata.GameData;
+import ooga.util.DukeApplicationTest;
+import ooga.util.parsers.ParserTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class GameDataInitializerTest extends ParserTest {
+
+  @Test
+  void TestInitializeGameData() throws ImproperlyFormattedFile {
+    GameData data = GameDataInitializer.generateGameData("monopoly_original");
+    assertEquals(data.getBoard().getTiles().size(),40);
+  }
+}
