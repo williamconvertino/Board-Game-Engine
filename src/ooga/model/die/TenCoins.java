822cb0bc4fb663ef5d3dc1f3adbf97141f28fe60
William Convertino <wac20@duke.edu>
2021-12-07 00:16:20 -0500

    Added tests

---
+++ src/ooga/model/die/TenCoins.java
@@ -0,0 +1,41 @@
+package ooga.model.die;
+
+import java.util.Random;
+
+/**
+ * An implementation of the Die class that flips 10 coins and
+ * moves based on the number of heads. Doubles are achieved if
+ * at least half of them are heads.
+ *
+ * @author William Convertino
+ *
+ * @since 1.0.1
+ */
+public class TenCoins extends Die {
+
+  /**
+   * Constructs a new TenCoins die.
+   */
+  public TenCoins() {
+    super(10);
+    super.lastRollDouble = false;
+    this.myLastRoll = new int[10];
+  }
+
+  /**
+   * @see Die#roll()
+   */
+  @Override
+  public int roll() {
+    Random myRandom = new Random();
+    int total = 0;
+    for (int i = 0; i < 10; i++) {
+      int flip = myRandom.nextInt(2);
+      total += flip;
+      myLastRoll[i] = flip;
+    }
+    lastRollDouble = total>=5;
+    return total;
+  }
+
+}
