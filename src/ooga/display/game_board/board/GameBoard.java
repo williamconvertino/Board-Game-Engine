d2c5925564ae32b535618b48422b93505e341a35
Casey Goldstein <cmg95@duke.edu>
2021-12-12 11:30:55 -0500

    fixed languages

--- src/ooga/display/game_board/board/GameBoard.java
+++ src/ooga/display/game_board/board/GameBoard.java
@@ -190,8 +190,6 @@ public class GameBoard {
     tileBox.setMaxWidth(width);
     tileBox.setMinHeight(height);
     tileBox.setMaxHeight(height);
-    System.out.println(tileIndex);
-    System.out.println(gameData.getBoard().getTileAtIndex(tileIndex).getName());
     String tileName = gameData.getBoard().getTileAtIndex(tileIndex).getName();
     String tileType = gameData.getBoard().getTileAtIndex(tileIndex).getMyType();
     Label propName = new Label(tileName);
